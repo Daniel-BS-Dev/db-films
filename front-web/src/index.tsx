460006bda8caf737c6bb473cb05673c5df1701d3
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-01-10 12:00:10 -0300

    Bootstrap

--- front-web/src/index.tsx
+++ front-web/src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
