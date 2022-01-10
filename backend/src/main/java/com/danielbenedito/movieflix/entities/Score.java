f745ad3fc8fad73ebd9a3f36303f70c5b531d9f7
Daniel-BS-Dev <danielbenedito355@gmail.com>
2022-01-10 18:46:50 -0300

    Anotation JPA

--- backend/src/main/java/com/danielbenedito/movieflix/entities/Score.java
+++ backend/src/main/java/com/danielbenedito/movieflix/entities/Score.java
@@ -2,11 +2,18 @@ package com.danielbenedito.movieflix.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import com.danielbenedito.movieflix.entities.pk.ScorePK;
 
+@Entity
+@Table(name="tb_score")
 public class Score implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@EmbeddedId
 	private ScorePK id = new ScorePK();
 	private Double value;
 	
--- backend/src/main/java/com/danielbenedito/movieflix/entities/pk/ScorePK.java
+++ backend/src/main/java/com/danielbenedito/movieflix/entities/pk/ScorePK.java
@@ -2,13 +2,24 @@ package com.danielbenedito.movieflix.entities.pk;
 
 import java.io.Serializable;
 
+import javax.persistence.Embeddable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 import com.danielbenedito.movieflix.entities.Movie;
 import com.danielbenedito.movieflix.entities.User;
 
+@Embeddable
 public class ScorePK implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@ManyToOne
+	@JoinColumn(name="user_id")
 	private User user;
+	
+
+	@ManyToOne
+	@JoinColumn(name="movie_id")
 	private Movie movie;
 	
 	public ScorePK() {
